9971cdf6c08fe94ee92c0c6d9d686351efa5b5bb
MauroTapia <maurotapiadev@gmail.com>
2023-07-07 19:31:18 -0300

    tercer commit

---
+++ C7-PatronFlyweight/src/Main.java
@@ -0,0 +1,18 @@
+import presencial.Bosque;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        final Bosque forest = new Bosque();
+
+            for (int i = 0; i < 1000000; i++) {
+                forest.plantarArbol("ORNAMENTALES", 200, 400, "verde");
+                forest.plantarArbol("FRUTALES", 500, 300, "rojo");
+                forest.plantarArbol("FRUTALES", 100, 200, "celestes");
+            }
+
+            Runtime runtime = Runtime.getRuntime();
+            System.out.println("Memoria usada: " + (runtime.totalMemory() - runtime.freeMemory()) / (1024 * 1024));
+    }
+}
---
+++ C7-PatronFlyweight/src/presencial/Bosque.java
@@ -0,0 +1,15 @@
+package presencial;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Bosque {
+    private static final List<Arbol> arboles= new ArrayList<>();
+    public void plantarArbol(final String tipoArbol, final int alto, final int ancho, final String color) {
+        final Arbol arbol = ArbolFactory.obtenerArbol(tipoArbol, alto, ancho, color);
+        arboles.add(arbol);
+    }
+    public List<Arbol>  getArboles(){
+        return arboles;
+    }
+}
