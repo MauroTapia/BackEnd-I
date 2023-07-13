9bdb25be3bffd9eae6573681bcaee3ad9b97e222
MauroTapia <maurotapiadev@gmail.com>
2023-07-13 19:28:18 -0300

    Thursday 13 July third

---
+++ C25ApiRest/src/main/java/com/example/clase23/controller/PacienteController.java
@@ -0,0 +1,22 @@
+package com.example.clase23.controller;
+
+import com.example.clase23.model.Paciente;
+import com.example.clase23.service.PacienteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/pacientes")
+public class PacienteController {
+    private PacienteService pacienteService;
+    @Autowired
+    public PacienteController(PacienteService pacienteService) {
+        this.pacienteService = pacienteService;
+    }
+    @PostMapping
+    public Paciente registrarPaciente(@RequestBody Paciente paciente){
+        return pacienteService.guardarPaciente(paciente);
+    }
+}
